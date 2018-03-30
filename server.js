caa5d80d0209bd639b2826a03dc5b72526f1d375
Rachel Elam <relam24@gmail.com>
2018-03-30 16:18:51 -0400

    added travel controller to server.js as well as mongoose

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+app.use(express.json());
+app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-  res.send('heyyyyyyyy')
+const travelController = require('./controllers/travel.js');
+app.use('/travel', travelController);
+
+mongoose.connect('mongodb://localhost:27017/travelApp');
+mongoose.connection.once('open', () => {
+	console.log('connected to mongod');
 });
 
-app.listen(3000, ()=>{
-  console.log('listening');
-})
+app.listen(3000, () => {
+	console.log('listening');
+});
