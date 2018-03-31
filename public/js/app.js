475d946a9c43ef29141cb601e6eae78e94380d9b
Natalie Wolff <natalielynnwolff@gmail.com>
2018-03-30 20:03:40 -0400

    public folder - post route works

---
+++ public/js/app.js
@@ -0,0 +1,62 @@
+const app = angular.module('travelApp', []);
+
+app.controller('MyController', ['$http', function($http){
+  const controller = this;
+
+  this.createTravel = function(){
+    $http({
+      method:'POST',
+      url:'/travel',
+      data: {
+        location: this.location,
+        url: this.url
+      }
+    }). then(function(response){
+      controller.getTravel();
+    }, function(){
+      console.log('error');
+    });
+  };
+
+  // this.deleteTravel = function(travel){
+  //   $http({
+  //     method:'DELETE',
+  //     url: '/travel' + travel._id
+  //   }).then(
+  //     function(response){
+  //       controller.getTravel();
+  //     },
+  //     function(error){
+  //
+  //     }
+  //   );
+  // }
+
+  this.getTravel = function(){
+    $http({
+      method: 'GET',
+      url:'/travel',
+    }).then(function(response){
+      controller.travel = response.data
+    }, function(){
+      console.log('error');
+    });
+  };
+this.getTravel();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}]); //closes app.controller
